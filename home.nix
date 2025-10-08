1abf0d4767b32ce4f0e8f8a27538f018738814fc
RuidiH <764342051@qq.com>
2025-10-07 23:13:27 -0700

    change starship preset to gruvbox rainbow

--- home.nix
+++ home.nix
@@ -33,23 +33,13 @@
 
 programs.starship = {
   enable = true;
-  enableZshIntegration = true;
-  settings = {
-    aws.style = "bold #ffb86c";
-    cmd_duration.style = "bold #f1fa8c";
-    directory.style = "bold #50fa7b";
-    hostname.style = "bold #ff5555";
-    git_branch.style = "bold #ff79c6";
-    git_status.style = "bold #ff5555";
-    username = {
-      format = "[$user]($style) on ";
-      style_user = "bold #bd93f9";
+
+  settings =
+    presetFile = pkgs.fetchurl {
+      url = "https://starship.rs/presets/toml/gruvbox-rainbow.toml";
+      sha256 = "sha256-eSIlVW89801BlI5d1VpAd2l2AX5trG43o1s62931uzE=";
     };
-    character = {
-      success_symbol = "[λ](bold #f8f8f2)";
-      error_symbol = "[λ](bold #ff5555)";
-    };
-  };
+    preset = builtins.fromTOML (builtins.readFile presetFile);
 };
 
 
